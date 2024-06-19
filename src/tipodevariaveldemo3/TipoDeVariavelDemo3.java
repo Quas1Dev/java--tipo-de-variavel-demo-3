306a0916e13ae49ee5a25aa9023b1923a75fa149
Fernando Bonfim <fernando.j.bonfim@gmail.com>
2024-06-19 19:31:29 -0300

    Initial file

---
+++ src/tipodevariaveldemo3/TipoDeVariavelDemo3.java
@@ -0,0 +1,17 @@
+package tipodevariaveldemo3;
+
+public class TipoDeVariavelDemo3 {
+
+    public static void main(String[] args) {
+        char c1, c2;
+
+        // O número 88 é o código para X em unicode.
+        c1 = 88;
+
+        c2 = 'Y';
+
+        System.out.println("c1: " + c1);
+        System.out.println("c2: " + c2);
+    }
+
+}
